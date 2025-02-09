172571e2dbeba095e77278e2b0cc0d4d84269b13
AfterBrun <96671007+AfterBrun@users.noreply.github.com>
2025-02-09 20:52:05 +0900

    Asset 변경 및 플레이어 애니메이션 추가

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -15,21 +15,44 @@ public class Player : MonoBehaviour
     private float _lastShootTime = 0.0f;
 
     [SerializeField]
+    private float _speed = 0.5f;
+
+    [SerializeField]
     public int _hp = 3;
 
+    Animator _animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
+    private void Awake() {
+        _animator = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.x = Mathf.Clamp(mousePosition.x, -2.5f, 2.5f);
 
         transform.position = new Vector3(mousePosition.x, transform.position.y, transform.position.z);
+        */
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
+        transform.position += moveTo * _speed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            _animator.SetBool("IsLeft", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow)) {
+            _animator.SetBool("IsLeft", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+            _animator.SetBool("IsRight", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.RightArrow)) {
+            _animator.SetBool("IsRight", false);
+        }
+
         shoot();
     }
 
